eb438fe54862dcbf5c267144fea3fd939672bbee
Brendan Nee <brendan@blinktag.com>
2021-01-31 00:50:49 -0800

    Webapp charts

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ const gtfs = require('gtfs');
 
 const express = require('express');
 const logger = require('morgan');
+const toposort = require('toposort');
+const { groupBy, last, uniqBy } = require('lodash');
 
 const utils = require('../lib/utils');
 const selectedConfig = require('../config');
@@ -29,15 +31,74 @@ gtfs.openDb(config).catch(error => {
 /*
  * Show all timetable pages
  */
-router.get('/', (request, response) => {
-  response.render('index');
+router.get('/', async (request, response, next) => {
+  try {
+    const routes = await gtfs.getRoutes({}, ['route_id', 'route_short_name', 'route_long_name'], [['route_short_name', 'ASC']]);
+
+    const routesWithDirections = await Promise.all(routes.map(async route => {
+      const trips = await gtfs.getTrips({
+        route_id: route.route_id
+      }, [
+        'trip_headsign',
+        'direction_id'
+      ], [
+        ['direction_id', 'ASC']
+      ]);
+      route.directions = uniqBy(trips, trip => trip.direction_id);
+
+      return route;
+    }));
+
+    response.render('index', { routes: routesWithDirections });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/api/board-alight', async (request, response, next) => {
+router.get('/api/board-alights', async (request, response, next) => {
   try {
-    const boardAlights = await gtfs.getBoardAlight();
+    const db = gtfs.getDb();
+    const values = [request.query.route_id];
 
-    return response.json(boardAlights);
+    let directionQuery = '';
+    if (request.direction_id !== 'null') {
+      directionQuery = 'AND direction_id = ?';
+      values.push(Number.parseInt(request.query.direction_id, 10));
+    }
+
+    const boardAlights = await db.all(`SELECT board_alight.trip_id, route_id, stop_id, boardings, alightings, load_count, service_date FROM board_alight LEFT JOIN trips ON board_alight.trip_id = trips.trip_id WHERE route_id = ? ${directionQuery} ORDER BY stop_sequence ASC`, values);
+
+    // Use a directed graph to determine stop order.
+    const tripGroups = groupBy(boardAlights, 'trip_id');
+    const stopGraph = [];
+
+    for (const tripGroup of Object.values(tripGroups)) {
+      const sortedStopIds = [];
+
+      for (const boardAlight of tripGroup) {
+        if (last(sortedStopIds) !== boardAlight.stop_id) {
+          sortedStopIds.push(boardAlight.stop_id);
+        }
+      }
+
+      for (const [index, stopId] of sortedStopIds.entries()) {
+        if (index === sortedStopIds.length - 1) {
+          continue;
+        }
+
+        stopGraph.push([stopId, sortedStopIds[index + 1]]);
+      }
+    }
+
+    const routeStopOrder = toposort(stopGraph);
+
+    const stops = await gtfs.getStops({ stop_id: routeStopOrder }, ['stop_id', 'stop_name']);
+
+    return response.json({
+      routeStopOrder,
+      stops,
+      boardAlights
+    });
   } catch (error) {
     next(error);
   }
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,174 @@
-/* global fetch */
+/* global document, fetch, Chart, routes */
 
-let boardAlights;
+let routeId;
+let directionId;
+let boardAlightData;
+const routeSelect = document.querySelector('#route-select');
+const directionSelect = document.querySelector('#direction-select');
 
-async function getBoardAlights() {
-  boardAlights = await fetch('/api/routes').then(response => response.json());
+const chart = new Chart('gtfs-ride-chart', {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Boardings',
+      backgroundColor: 'rgb(75, 192, 192)',
+      borderColor: 'rgb(75, 192, 192)',
+      data: [],
+      lineTension: 0,
+      fill: false
+    }, {
+      label: 'Alightings',
+      fill: false,
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [],
+      lineTension: 0
+    }]
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: 'Ridership'
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Stop'
+        }
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Riders'
+        }
+      }]
+    }
+  }
+});
 
-  console.log(boardAlights);
+function formatRouteName(route) {
+  const nameParts = [];
+
+  if (route.route_short_name !== '' && route.route_short_name !== null) {
+    nameParts.push(route.route_short_name);
+  }
+
+  if (route.route_long_name !== '' && route.route_long_name !== null) {
+    nameParts.push(route.route_long_name);
+  }
+
+  return nameParts.join(' - ');
 }
 
-getBoardAlights();
+function formatDirectionName(direction) {
+  const nameParts = [];
+
+  if (direction.direction_id !== '' && direction.direction_id !== null) {
+    nameParts.push(direction.direction_id);
+  } else {
+    nameParts.push('None');
+  }
+
+  if (direction.trip_headsign !== '' && direction.trip_headsign !== null) {
+    nameParts.push(direction.trip_headsign);
+  }
+
+  return nameParts.join(' - ');
+}
+
+async function getBoardAlights() {
+  boardAlightData = await fetch(`/api/board-alights?route_id=${routeId}&direction_id=${directionId}`).then(response => response.json());
+}
+
+function updateChart() {
+  const route = routes.find(route => route.route_id === routeId);
+
+  if (boardAlightData.routeStopOrder.length === 0) {
+    alert(`No ridership data available for Route ${formatRouteName(route)}.`);
+  }
+
+  const data = boardAlightData.routeStopOrder.map(stopId => {
+    const stop = boardAlightData.stops.find(stop => stop.stop_id === stopId);
+    return {
+      label: stop.stop_name,
+      stop_id: stop.stop_id,
+      boardings: 0,
+      alightings: 0,
+      load_count: 0
+    };
+  });
+
+  for (const boardAlight of boardAlightData.boardAlights) {
+    const dataPoint = data.find(item => item.stop_id === boardAlight.stop_id);
+
+    if (boardAlight.boardings !== null) {
+      dataPoint.boardings += boardAlight.boardings;
+    }
+
+    if (boardAlight.alightings !== null) {
+      dataPoint.alightings += boardAlight.alightings;
+    }
+
+    if (boardAlight.load_count !== null) {
+      dataPoint.load_count += boardAlight.load_count;
+    }
+  }
+
+  chart.data.labels = data.map(item => item.label);
+  chart.data.datasets[0].data = data.map(item => item.boardings);
+  chart.data.datasets[1].data = data.map(item => item.alightings);
+  chart.options.title.text = `Ridership on Route ${formatRouteName(route)}, direction ${directionId}`;
+
+  chart.update();
+}
+
+routeSelect.addEventListener('change', event => {
+  directionSelect.length = 0;
+  if (event.target.value === '') {
+    directionSelect.parentElement.classList.add('hidden');
+    return;
+  }
+
+  routeId = event.target.value;
+
+  const route = routes.find(route => route.route_id === routeId);
+
+  const option = document.createElement('option');
+  option.text = 'Select a direction';
+  option.value = '';
+  directionSelect.append(option);
+
+  for (const direction of route.directions) {
+    const option = document.createElement('option');
+    option.text = formatDirectionName(direction);
+    option.value = direction.direction_id === null ? 'null' : direction.direction_id;
+    directionSelect.append(option);
+  }
+
+  directionSelect.parentElement.classList.remove('hidden');
+});
+
+directionSelect.addEventListener('change', async event => {
+  if (event.target.value === '') {
+    return;
+  }
+
+  directionId = event.target.value;
+
+  boardAlights = await getBoardAlights();
+
+  updateChart();
+});
