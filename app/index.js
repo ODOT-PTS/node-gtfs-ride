d1e0121d5aa3f2bb75c5933388d4887987bba147
Brendan Nee <brendan@blinktag.com>
2021-02-17 22:15:51 -0800

    Support for port in config.json

--- app/index.js
+++ app/index.js
@@ -119,7 +119,7 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', router);
-app.set('port', process.env.PORT || 3000);
+app.set('port', selectedConfig.port || process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server listening on port ${server.address().port}`);
